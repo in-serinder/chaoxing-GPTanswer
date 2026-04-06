26ad3d9659e191aa09411364f2e302cf72ea0de9
Serinder <serinder@163.com>
2026-04-06 12:33:05 +0800

    挂载测试

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -21,13 +21,13 @@ export default defineConfig({
       fileName: () => "index.js",
     },
     rollupOptions: {
-      external: ["vue", "pinia", "'vue-demi'"], // 由油猴脚本提供
+      // external: ["vue", "pinia", "'vue-demi'"], // 由油猴脚本提供
       output: {
-        globals: {
-          vue: "Vue",
-          pinia: "Pinia",
-          "vue-demi": "VueDememi",
-        },
+        // globals: {
+        //   vue: "Vue",
+        //   pinia: "Pinia",
+        //   "vue-demi": "VueDememi",
+        // },
       },
     },
   },

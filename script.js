1e43b8d0aba4cd0ed05c02d944112fbe02d228d5
Serinder <serinder@163.com>
2026-04-07 10:18:41 +0800

    油猴挂载通过

--- script.js
+++ script.js
@@ -11,24 +11,36 @@
 (function () {
     'use strict';
 
-    // 动态添加 CSS
-    const cssLink = document.createElement('link');
-    cssLink.rel = 'stylesheet';
-    cssLink.href = 'https://cdn.jsdelivr.net/gh/in-serinder/chaoxing-GPTanswer@main/ui/dist/ui.css';
-    document.head.appendChild(cssLink);
+    const MOUNTPOINT_CSS = 'https://cdn.jsdelivr.net/gh/in-serinder/chaoxing-GPTanswer@main/ext-style/mount-point.css';
+    const UI_CSS = 'https://cdn.jsdelivr.net/gh/in-serinder/chaoxing-GPTanswer@main/ui/dist/ui.css';
 
-    // 动态添加 JavaScript
-    const script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/gh/in-serinder/chaoxing-GPTanswer@main/mount-test/index.js';
-    script.onload = function () {
-        console.log('脚本加载完成');
-        // 执行挂载操作
-        if (unsafeWindow.ChaoxingGPT) {
-            const mountPoint = document.createElement('div');
-            mountPoint.id = 'chaoxing-gpt-app';
-            document.body.appendChild(mountPoint);
-            unsafeWindow.ChaoxingGPT.mount(mountPoint);
-        }
-    };
-    document.head.appendChild(script);
+    const INDEX_JS = 'https://cdn.jsdelivr.net/gh/in-serinder/chaoxing-GPTanswer@main/mount-test/index.js';
+
+    function loadcss(url) {
+        const cssLink = document.createElement('link');
+        cssLink.rel = 'stylesheet';
+        cssLink.href = url;
+        document.head.appendChild(cssLink);
+    }
+
+
+    function loadjs(url) {
+        const script = document.createElement('script');
+        script.src = url;
+        script.onload = function () {
+            console.log('脚本加载完成');
+            if (unsafeWindow.ChaoxingGPT) {
+                const mountPoint = document.createElement('div');
+                mountPoint.id = 'chaoxing-gpt-app';
+                document.body.appendChild(mountPoint);
+                unsafeWindow.ChaoxingGPT.mount(mountPoint);
+            }
+        };
+        document.head.appendChild(script);
+    }
+
+    loadcss(MOUNTPOINT_CSS);
+    loadcss(UI_CSS);
+    loadjs(INDEX_JS);
+
 })();
